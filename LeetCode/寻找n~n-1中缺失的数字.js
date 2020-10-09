ef66b36905c9344010f888b6fdc46f30cc503d99
pudding <bdingtech@gmail.com>
2020-10-09 17:10:27 +0800

    add:寻找n-1中缺失的数字(二分查找法)

--- LeetCode/寻找n~n-1中缺失的数字.js
+++ LeetCode/寻找n~n-1中缺失的数字.js
@@ -1,4 +1,5 @@
 /**
+ * 暴力破解法
  * @param {number[]} nums
  * @return {number}
  */
@@ -7,4 +8,19 @@ var missingNumber = function(nums) {
         if(nums[i] !== i)
         return i
     }
+};
+
+/**
+ * 二分查找法
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let low = 0
+    let hig = nums.length -1
+    while(low <= hig){
+        let mid = parseInt(low + (hig - low) /2)
+        nums[mid] == mid ? low = mid + 1 : hig = mid -1
+    }
+    return low
 };
