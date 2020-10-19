09ff33da45c649830cc9d01bed22d4157c7d0f5d
pudding <bdingtech@gmail.com>
2020-10-20 00:28:32 +0800

    update:reademe

--- LeetCode/找到所有数组中消失的数字.js
+++ LeetCode/找到所有数组中消失的数字.js
@@ -4,10 +4,14 @@
  */
 var findDisappearedNumbers = function(nums) {
     //创建一个1-n的数组
-    let arr = []
-    for(let i = 1; i <= nums.length; i++){
-        arr.push(i)
-    }
+    let arr = Array.from({length:nums.length}).map((item,idx) => (idx + 1))
     nums.map(item => {arr[item - 1] = null})
     return arr.filter(item => (item))
 };
+
+/**
+ * 得到
+ * 1.Array.from可以根据有length属性的对象返回一个数组
+ * 2.这种先生成数组，然后再打标记的方法比较秀，但是好像违背了题目不能额外创建空间的规定
+ * 
+ */
