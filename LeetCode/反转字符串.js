96e65eb54f73ecb9ccf480c88003a1797f177473
bdingtech <bdingtech@gmail.com>
2020-12-10 13:44:19 +0800

    add:反转字符串(使用解构赋值)

--- LeetCode/反转字符串.js
+++ LeetCode/反转字符串.js
@@ -12,3 +12,14 @@ var reverseString = function (s) {
     }
     return s
 };
+
+/**
+ * 使用解构赋值
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    for (let i = 0, left = 0, right = s.length - 1; left < right; i++,left++,right--) {
+        [s[left], s[right]] = [s[right], s[left]] //作为工具使用
+    }
+};
