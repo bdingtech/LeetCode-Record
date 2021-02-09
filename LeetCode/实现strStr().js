842fed8cd948ca5f274645198f4b1eeb99a47d43
bdingtech <bdingtech@gmail.com>
2021-02-09 21:40:02 +0800

    feat: 新增 实现strStr() (暴力穷举法)
    \

--- LeetCode/实现strStr().js
+++ LeetCode/实现strStr().js
@@ -33,3 +33,35 @@ var strStr = function(haystack, needle) {
     }
     return -1
 };
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    const hayLen = haystack.length
+    const nedLen = needle.length
+    if(nedLen > hayLen){
+        return -1
+    }
+    if(nedLen === 0){
+        return 0
+    }
+    if(nedLen === hayLen){
+        return needle === haystack ? 0 : -1
+    }
+    for(let i = 0; i <= hayLen - nedLen; i++){
+        for(let j = 0; j < nedLen; j++){
+            //不相等的话直接跳出此次循环
+            if(haystack[i + j] !== needle[j]){
+                break
+            }
+            //到最后一位还相等的话,就返回第一位
+            if(j == nedLen - 1 ){
+                return i
+            }
+        }
+    }
+    return -1
+};
