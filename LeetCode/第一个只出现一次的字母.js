35ad7be0f88c7da8d90608cd243312cd190eec01
bdingtech <bdingtech@gmail.com>
2021-02-09 17:55:47 +0800

    feat: 新增 第一次只出现一次的字母

---
+++ LeetCode/第一个只出现一次的字母.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var firstUniqChar = function(s) {
+    for(let i = 0 ; i < s.length; i++){
+        if(s.indexOf(s[i]) === s.lastIndexOf(s[i])){
+            return s[i]
+        }
+    }
+    return " "
+};
