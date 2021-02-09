1ca65839227a73f1917542744a084984cf15b04b
bdingtech <bdingtech@gmail.com>
2021-02-09 18:04:26 +0800

    feat: 新增 第一次只出现一个字母(哈希表解法)

--- LeetCode/第一个只出现一次的字母.js
+++ LeetCode/第一个只出现一次的字母.js
@@ -10,3 +10,26 @@ var firstUniqChar = function(s) {
     }
     return " "
 };
+
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var firstUniqChar = function(s) {
+    let hash = {}
+    //第一次遍历，标记每个字母出现的次数
+    for(let i = 0; i < s.length; i++ ){
+        if(!hash[s[i]]){
+            hash[s[i]] = 1
+        }else{
+            hash[s[i]]++
+        }
+    }
+    //第二次遍历 找出标记为1的字符
+    for(item in hash){
+        if(hash[item] === 1){
+            return item
+        }
+    }
+    return " "
+};
