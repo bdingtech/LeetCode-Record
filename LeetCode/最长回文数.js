b69f94d98caac614ee4815233a83d4b4c381861f
bdingtech <bdingtech@gmail.com>
2021-02-10 14:47:28 +0800

    feat: 新增 最长回文数(计数解法)

--- LeetCode/最长回文数.js
+++ LeetCode/最长回文数.js
@@ -15,3 +15,29 @@ var longestPalindrome = function(s) {
     }
     return hash.size >= 1 ? count + 1 : count
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    //将每个字符存入对象中
+    let hash = {}
+    let count = 0
+    for(let i = 0; i < s.length; i++){
+        if(!hash[s[i]]){
+            hash[s[i]] = 1
+        }else{
+            hash[s[i]] += 1
+            count+=2
+            hash[s[i]] = 0
+        }
+    }
+    let flag = 0
+    for(item in hash){
+        if(hash[item] >= 1){
+            flag++
+        }
+    }
+    return flag >= 1 ? count + 1 : count
+};
