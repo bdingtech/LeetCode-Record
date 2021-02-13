fe284cb30f0b707c31400c709c91258565d3e24b
bdingtech <bdingtech@gmail.com>
2021-02-13 16:08:20 +0800

    feat: 新增IP地址无效化(正则解法)

--- LeetCode/IP地址无效化.js
+++ LeetCode/IP地址无效化.js
@@ -6,4 +6,12 @@ var defangIPaddr = function(address) {
     return address.split('.').join('[.]')
 };
 
+/**
+ * 正则解法
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    return address.replace(/\./g,"[.]")
+};
 // 第一个10秒钟搞定的题，哈哈哈，虽然足够简单
