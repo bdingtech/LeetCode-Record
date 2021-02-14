21177907a5ec331062be282e143635c81c28f485
bdingtech <bdingtech@gmail.com>
2021-02-14 20:26:58 +0800

    feat: 新增 数组中重复的数

--- LeetCode/数组中重复的数.js
+++ LeetCode/数组中重复的数.js
@@ -1,32 +1,13 @@
 /**
- * 第一代
- * 但是sort好像未被了O(n)的规定
  * @param {number[]} nums
- * @return {number[]}
+ * @return {number}
  */
-var findDuplicates = function(nums) {
-    nums.sort((a , b) => (a - b))
-    let arr = []
-    nums.map((item,idx) => {nums[idx] - nums[idx-1] == 0 ? arr.push(nums[idx-1]): ''})
-    return arr
-};
-
-
-/**
- * 第二代
- * 没看太懂，脑壳疼
- * @param {number[]} nums
- * @return {number[]}
- */
-var findDuplicates = function(nums) {
-    let result = []
-    for(let i = 0; i < nums.length; i++ ){
-        let num = Math.abs(nums[i])
-        if(nums[num - 1] > 0 ){
-            nums[num -1] *= -1
-        }else{
-            result.push(num)
+var findRepeatNumber = function(nums) {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] === nums[j]){
+                return nums[i]
+            }
         }
     }
-    return result
 };
