a9a322f860992eac042652e186cb896e86266808
bdingtech <bdingtech@gmail.com>
2021-02-14 18:38:32 +0800

    feat: 新增 左旋字符串

--- LeetCode/左旋转字符串.js
+++ LeetCode/左旋转字符串.js
@@ -6,3 +6,11 @@
 var reverseLeftWords = function(s, n) {
     return s.split('').slice(n).join('') + s.split('').splice(0,n).join('') 
 };
+/**
+ * @param {string} s
+ * @param {number} n
+ * @return {string}
+ */
+var reverseLeftWords = function(s, n) {
+    return s.slice(n) + s.slice(0,n) 
+};
