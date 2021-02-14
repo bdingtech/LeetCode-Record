efcae5311ea419277228b4a763fc17a7d448b38d
bdingtech <bdingtech@gmail.com>
2021-02-14 19:13:31 +0800

    feat: 新增 宝石与石头(set解法)

--- LeetCode/宝石与石头.js
+++ LeetCode/宝石与石头.js
@@ -5,10 +5,21 @@
  */
 var numJewelsInStones = function(jewels, stones) {
     let count = 0
-    stones.split('').forEach((item,index) => {
-        if(jewels.indexOf(item) !== -1){
-            count++
-        }
-    })
+    for(item of stones){
+        jewels.indexOf(item) !== -1 && count++
+    }
+    return count
+};
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    let count = 0
+    let hash = new Set(jewels)
+    for(item of stones){
+        hash.has(item) && count++
+    }
     return count
 };
