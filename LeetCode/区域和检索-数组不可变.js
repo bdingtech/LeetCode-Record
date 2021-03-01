5d4d6877400774eb4f906a5922949b449fc6229c
bdingtech <bdingtech@gmail.com>
2021-03-01 22:11:08 +0800

    feat: 新增 区域和检索-数组不可变

---
+++ LeetCode/区域和检索-数组不可变.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = nums
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    let sum = 0
+    for(let k = i; k <= j; k++){
+        sum+=this.nums[k]
+    }
+    return sum
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
